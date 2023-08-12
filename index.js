69f32c6239d3bf1b21ccae79aa9f5e6202df2ce1
MDwornikowski <marcin.m.dwornikowski@gmail.com>
2023-08-12 17:17:47 +0200

    Working on JS for nesting dropdown lists in the sidebar menu

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Table of contents - Dropdown list
     const items = document.getElementById("tableOfContents").querySelectorAll(".item");
     const itemsWithDropdown = document.getElementById("tableOfContents").querySelectorAll('.has-dropdown');
+    var itemsWithDropdownDropdownHeights = [];
 
     items.forEach(item => {
         item.addEventListener("click", (event) => {
@@ -19,10 +20,38 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    itemsWithDropdown.forEach(item => {
+    // itemsWithDropdown.forEach(item => {
+    //     const nestedDropdown = item.querySelector('.nested-dropdown');
+
+    //     var nextSibling = item.nextElementSibling;
+    //     if (nextSibling == null) {
+    //         nextSibling = getParentsNextSiblingRecursively(item);
+    //     }
+
+    //     item.querySelector(".dropdown-button").addEventListener('click', () => {
+    //         nestedDropdown.classList.toggle('active');
+
+    //         if (nestedDropdown.classList.contains('active')) {
+    //             nextSibling.style.marginTop = `${nestedDropdown.clientHeight}px`;
+    //         } else {
+    //             nextSibling.style.marginTop = '0';
+    //         }
+    //     });
+    // });
+
+    for (let i = 0; i < itemsWithDropdown.length; i++) {
+        const item = itemsWithDropdown[i];
         const nestedDropdown = item.querySelector('.nested-dropdown');
 
-        var nextSibling = item.nextElementSibling;
+        // Add dropdown heights to their array:
+        const dropdownHeightObj = {
+            Item: item,
+            Height: `${nestedDropdown.clientHeight}px`
+        };
+        itemsWithDropdownDropdownHeights.push(dropdownHeightObj);
+
+        // Add 'click' event to itemsWithDropdown:
+        let nextSibling = item.nextElementSibling;
         if (nextSibling == null) {
             nextSibling = getParentsNextSiblingRecursively(item);
         }
@@ -31,12 +60,14 @@ document.addEventListener("DOMContentLoaded", function () {
             nestedDropdown.classList.toggle('active');
 
             if (nestedDropdown.classList.contains('active')) {
-                nextSibling.style.marginTop = `${nestedDropdown.clientHeight}px`;
+                // nextSibling.style.marginTop = `${nestedDropdown.clientHeight}px`;
+                console.log(nestedDropdown);
+                nextSibling.style.marginTop = getDropdownTotalHeightRecursively(nestedDropdown);
             } else {
                 nextSibling.style.marginTop = '0';
             }
         });
-    });
+    }
 
     function getParentsNextSiblingRecursively(element) {
         var parent = element.parentElement;
@@ -45,5 +76,60 @@ document.addEventListener("DOMContentLoaded", function () {
             return getParentsNextSiblingRecursively(parent);
         else
             return nextSibling;
+    }
+
+    function getDropdownTotalHeightRecursively(dropdown) {
+        // var parent = item.parentElement;
+        //var totalHeight = `${item.querySelector('.nested-dropdown').clientHeight}px`;
+        // const parentNestedDropdown = parent.querySelector('.nested-dropdown')
+        // if (parentNestedDropdown != null) {
+        //     totalHeight += parentNestedDropdown;
+        //     totalHeight += getParentsDropdownHeightsRecursively(parent);
+        // }
+        // else {
+        //     return totalHeight;
+        // }
+
+        var totalHeight = 0;
+
+        function traverseAndCalculateHeight(dropdown) {
+            totalHeight += dropdown.clientHeight;
+            var parent = dropdown.closest('.has-dropdown');
+            console.log(parent);
+            if (parent) {
+                var parentActiveDropdowns = [];
+                for (let i = 0; i < parent.children.length; i++) {
+                    var child = parent.children[i];
+                    if (child != dropdown) {
+                        if (child.classList.contains('nested-dropdown') && child.classList.contains('active')) {
+                            console.log(child);
+                            parentActiveDropdowns.push(child);
+                        }
+                    }
+                    else {
+                        break;
+                    }
+                }
+                console.log(parentActiveDropdowns.length);
+
+                if (parentActiveDropdowns) {
+                    for (let i = 0; i < parentActiveDropdowns.length; i++) {
+                        const parentActiveDropdown = parentActiveDropdowns[i];
+                        totalHeight += parentActiveDropdown.clientHeight;
+                    }
+                }
+
+                const parentNestedDropdown = parent.closest('.nested-dropdown');
+                if (parentNestedDropdown) {
+                    console.log(parentNestedDropdown);
+                    traverseAndCalculateHeight(parentNestedDropdown);
+                }
+
+            }
+        }
+
+        traverseAndCalculateHeight(dropdown);
+
+        return `${totalHeight}px`;
     }
 });
