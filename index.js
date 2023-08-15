27aa9a080bbdaa72a01f74dfcb7d81c02809a479
MDwornikowski <marcin.m.dwornikowski@gmail.com>
2023-08-15 11:47:49 +0200

    Testing different approaches to applying proper margins in the table of contents nesting tree

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const items = document.getElementById("tableOfContents").querySelectorAll(".item");
     const itemsWithDropdown = document.getElementById("tableOfContents").querySelectorAll('.has-dropdown');
     var itemsWithDropdownDropdownHeights = [];
+    var nestedDropdownsAndTheirSiblings = [];
 
+    // Stop event propagation (childs calling the event on parents):
     items.forEach(item => {
         item.addEventListener("click", (event) => {
-            event.stopPropagation(); // Stop event propagation (childs calling the event on parents)
+            event.stopPropagation();
         });
     });
 
@@ -50,35 +52,250 @@ document.addEventListener("DOMContentLoaded", function () {
         };
         itemsWithDropdownDropdownHeights.push(dropdownHeightObj);
 
-        // Add 'click' event to itemsWithDropdown:
-        let nextSibling = item.nextElementSibling;
+        var nextSibling = item.nextElementSibling;
+        let siblingLevelObj = {
+            SiblingLevel: 0
+            // SiblingLevel: []
+        }
+        // var siblings = [];
+        // siblings.push(nextSibling);
+        // siblingLevelObj.push(0);
+
         if (nextSibling == null) {
-            nextSibling = getParentsNextSiblingRecursively(item);
+            // siblingLevelObj.SiblingLevel++;
+            nextSibling = getParentsNextSiblingRecursively(item, siblingLevelObj);
+            // siblings = getParentsNextSiblingRecursively(item, siblingLevelObj);
         }
 
+        const dropdownSiblingObj = {
+            NestedDropdown: nestedDropdown,
+            Sibling: nextSibling,
+            // Sibling: siblings,
+            SiblingLevel: siblingLevelObj.SiblingLevel
+        };
+        console.log('Adding nested d:');
+        console.log(nestedDropdown);
+        console.log('Sibling:');
+        console.log(nextSibling);
+        console.log(siblingLevelObj.SiblingLevel);
+        nestedDropdownsAndTheirSiblings.push(dropdownSiblingObj);
+
+
+        // Add 'click' event to itemsWithDropdown:
         item.querySelector(".dropdown-button").addEventListener('click', () => {
             nestedDropdown.classList.toggle('active');
 
-            if (nestedDropdown.classList.contains('active')) {
-                // nextSibling.style.marginTop = `${nestedDropdown.clientHeight}px`;
-                console.log(nestedDropdown);
-                nextSibling.style.marginTop = getDropdownTotalHeightRecursively(nestedDropdown);
-            } else {
-                nextSibling.style.marginTop = '0';
+            // if (nestedDropdown.classList.contains('active')) {
+            //     // nextSibling.style.marginTop = `${nestedDropdown.clientHeight}px`;
+            //     console.log(nestedDropdown);
+            //     nextSibling.style.marginTop = getDropdownTotalHeightRecursively(nestedDropdown);
+            // } else {
+            //     nextSibling.style.marginTop = '0';
+            // }
+
+            calculateDropdownHeights();
+        });
+    }
+
+    // function getParentsNextSiblingRecursively(element) {
+    //     var parent = element.parentElement;
+    //     var nextSibling = parent.nextElementSibling;
+    //     if (nextSibling == null) {
+    //         return getParentsNextSiblingRecursively(parent);
+    //     }
+    //     else {
+    //         return nextSibling;
+    //     }
+    // }
+
+    function getParentsNextSiblingRecursively(element, siblingLevelObj) {
+        siblingLevelObj.SiblingLevel++;
+        var parent = element.parentElement;
+        // var parent = element.closest('.has-dropdown');
+        console.log(element);
+        console.log(parent);
+        var nextSibling = parent.nextElementSibling;
+        if (nextSibling == null) {
+            return getParentsNextSiblingRecursively(parent, siblingLevelObj);
+        }
+        else {
+            return nextSibling;
+        }
+    }
+
+    // function calculateDropdownHeights(itemWithDropdown, nextSibling) {
+    //     const activeNestedDropdowns = itemWithDropdown.querySelectorAll('.nested-dropdown.active');
+    //     const lastActiveDropdown = activeNestedDropdowns[activeNestedDropdowns.length - 1];
+    //     const recursiveHeight = getDropdownTotalHeightRecursively(dropdown.NestedDropdown);
+
+    //     let nextSibling = lastActiveDropdown.nextElementSibling;
+    //     if (nextSibling == null) {
+    //         nextSibling = getParentsNextSiblingRecursively(item);
+    //     }
+
+    //     dropdown.Sibling.style.marginTop = recursiveHeight;
+
+    // }
+
+    function calculateDropdownHeights() {
+        var siblingMargins = [];
+        nestedDropdownsAndTheirSiblings.forEach(dropdown => {
+            console.log('Dropdown:');
+            console.log(dropdown.NestedDropdown);
+
+            var siblingMarginSet = false;
+            for (let i = 0; i < siblingMargins.length; i++) {
+                if (siblingMargins[i].Sibling == dropdown.Sibling) {
+                    siblingMarginSet = true;
+                }
+            }
+
+            if (dropdown.NestedDropdown.classList.contains('active')) {
+                const recursiveHeight = getDropdownTotalHeightRecursively(dropdown.NestedDropdown, dropdown.SiblingLevel);
+                // dropdown.Sibling.style.marginTop = recursiveHeight;
+                if (!siblingMarginSet) {
+                    dropdown.Sibling.style.marginTop = recursiveHeight;
+                    const siblingMarginObj = {
+                        Sibling: dropdown.Sibling,
+                        Margin: dropdown.Sibling.style.marginTop
+                    };
+                    siblingMargins.push(siblingMarginObj);
+                }
+                else {
+                    if (recursiveHeight > dropdown.Sibling.style.marginTop) {
+                        dropdown.Sibling.style.marginTop = recursiveHeight;
+                    }
+                }
+                console.log('Active, Sibling:');
+                console.log(dropdown.Sibling);
+                console.log('Margin:');
+                console.log(dropdown.Sibling.style.marginTop);
+            }
+            else {
+                if (!siblingMarginSet) {
+                    dropdown.Sibling.style.marginTop = '0';
+                    console.log('Not active, Sibling:');
+                    console.log(dropdown.Sibling);
+                    console.log('Margin:');
+                    console.log(dropdown.Sibling.style.marginTop);
+                }
             }
         });
     }
 
-    function getParentsNextSiblingRecursively(element) {
-        var parent = element.parentElement;
-        var nextSibling = parent.nextElementSibling;
-        if (nextSibling == null)
-            return getParentsNextSiblingRecursively(parent);
-        else
-            return nextSibling;
+    // function getDropdownTotalHeightRecursively(dropdown, sibling) {
+    //     // var parent = item.parentElement;
+    //     //var totalHeight = `${item.querySelector('.nested-dropdown').clientHeight}px`;
+    //     // const parentNestedDropdown = parent.querySelector('.nested-dropdown')
+    //     // if (parentNestedDropdown != null) {
+    //     //     totalHeight += parentNestedDropdown;
+    //     //     totalHeight += getParentsDropdownHeightsRecursively(parent);
+    //     // }
+    //     // else {
+    //     //     return totalHeight;
+    //     // }
+
+    //     var totalHeight = 0;
+
+    //     function traverseAndCalculateHeight(dropdown, depth) {
+    //         totalHeight += dropdown.clientHeight;
+    //         var parent = dropdown.closest('.has-dropdown');
+    //         // console.log(parent);
+    //         if (parent) {
+    //             var parentActiveDropdowns = [];
+    //             for (let i = 0; i < parent.children.length; i++) {
+    //                 var child = parent.children[i];
+    //                 if (child != dropdown) {
+    //                     if (child.classList.contains('nested-dropdown') && child.classList.contains('active')) {
+    //                         // console.log(child);
+    //                         parentActiveDropdowns.push(child);
+    //                     }
+    //                 }
+    //                 else {
+    //                     break;
+    //                 }
+    //             }
+    //             // console.log(parentActiveDropdowns.length);
+
+    //             if (parentActiveDropdowns) {
+    //                 for (let i = 0; i < parentActiveDropdowns.length; i++) {
+    //                     const parentActiveDropdown = parentActiveDropdowns[i];
+    //                     totalHeight += parentActiveDropdown.clientHeight;
+    //                 }
+    //             }
+
+    //             const parentNestedDropdown = parent.closest('.nested-dropdown');
+    //             if (parent.nextElementSibling != && parentNestedDropdown) {
+    //                 // console.log(parentNestedDropdown);
+    //                 traverseAndCalculateHeight(parentNestedDropdown);
+    //             }
+
+    //         }
+    //     }
+
+    //     traverseAndCalculateHeight(dropdown, depth);
+
+    //     return `${totalHeight}px`;
+    // }
+
+    function getDropdownTotalHeightRecursively(dropdown, depth) {
+        // var parent = item.parentElement;
+        //var totalHeight = `${item.querySelector('.nested-dropdown').clientHeight}px`;
+        // const parentNestedDropdown = parent.querySelector('.nested-dropdown')
+        // if (parentNestedDropdown != null) {
+        //     totalHeight += parentNestedDropdown;
+        //     totalHeight += getParentsDropdownHeightsRecursively(parent);
+        // }
+        // else {
+        //     return totalHeight;
+        // }
+
+        var totalHeight = 0;
+        var currentDepth = 0;
+
+        function traverseAndCalculateHeight(dropdown, depth) {
+            currentDepth++;
+            totalHeight += dropdown.clientHeight;
+            var parent = dropdown.closest('.has-dropdown');
+            // console.log(parent);
+            if (parent) {
+                var parentActiveDropdowns = [];
+                for (let i = 0; i < parent.children.length; i++) {
+                    var child = parent.children[i];
+                    if (child != dropdown) {
+                        if (child.classList.contains('nested-dropdown') && child.classList.contains('active')) {
+                            // console.log(child);
+                            parentActiveDropdowns.push(child);
+                        }
+                    }
+                    else {
+                        break;
+                    }
+                }
+                // console.log(parentActiveDropdowns.length);
+
+                if (parentActiveDropdowns) {
+                    for (let i = 0; i < parentActiveDropdowns.length; i++) {
+                        const parentActiveDropdown = parentActiveDropdowns[i];
+                        totalHeight += parentActiveDropdown.clientHeight;
+                    }
+                }
+
+                const parentNestedDropdown = parent.closest('.nested-dropdown');
+                if (currentDepth < depth && parentNestedDropdown) {
+                    // console.log(parentNestedDropdown);
+                    traverseAndCalculateHeight(parentNestedDropdown, depth);
+                }
+
+            }
+        }
+
+        traverseAndCalculateHeight(dropdown, depth);
+
+        return `${totalHeight}px`;
     }
 
-    function getDropdownTotalHeightRecursively(dropdown) {
+    function getDropdownTotalHeightRecursively2(dropdown) {
         // var parent = item.parentElement;
         //var totalHeight = `${item.querySelector('.nested-dropdown').clientHeight}px`;
         // const parentNestedDropdown = parent.querySelector('.nested-dropdown')
@@ -95,14 +312,14 @@ document.addEventListener("DOMContentLoaded", function () {
         function traverseAndCalculateHeight(dropdown) {
             totalHeight += dropdown.clientHeight;
             var parent = dropdown.closest('.has-dropdown');
-            console.log(parent);
+            // console.log(parent);
             if (parent) {
                 var parentActiveDropdowns = [];
                 for (let i = 0; i < parent.children.length; i++) {
                     var child = parent.children[i];
                     if (child != dropdown) {
                         if (child.classList.contains('nested-dropdown') && child.classList.contains('active')) {
-                            console.log(child);
+                            // console.log(child);
                             parentActiveDropdowns.push(child);
                         }
                     }
@@ -110,7 +327,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         break;
                     }
                 }
-                console.log(parentActiveDropdowns.length);
+                // console.log(parentActiveDropdowns.length);
 
                 if (parentActiveDropdowns) {
                     for (let i = 0; i < parentActiveDropdowns.length; i++) {
@@ -121,7 +338,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 const parentNestedDropdown = parent.closest('.nested-dropdown');
                 if (parentNestedDropdown) {
-                    console.log(parentNestedDropdown);
+                    // console.log(parentNestedDropdown);
                     traverseAndCalculateHeight(parentNestedDropdown);
                 }
 
