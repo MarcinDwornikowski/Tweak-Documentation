9708b7e1d6020653d804802869295442f0f0977b
MDwornikowski <marcin.m.dwornikowski@gmail.com>
2023-09-09 20:14:25 +0200

    Squashed commit of the following:
    commit 6d757e042019919a35d61d91efb0c7e2f02058cc
    Author: MDwornikowski <marcin.m.dwornikowski@gmail.com>
    Date:   Fri Sep 1 23:31:24 2023 +0200
    
        Fixed the custom hover for buttons in the table of contents not applying the previous styling on mouseout
    
    commit 454347e9edf1ef7cf058590d01a6d61e389a9a0d
    Author: MDwornikowski <marcin.m.dwornikowski@gmail.com>
    Date:   Fri Sep 1 23:02:02 2023 +0200
    
        Changed the arrow icons in the table of contents
    
    commit f4a66d4335c75e569d7ece8e893c37dd1fee17f9
    Author: MDwornikowski <marcin.m.dwornikowski@gmail.com>
    Date:   Fri Sep 1 22:44:57 2023 +0200
    
        Code cleanup
    
    commit 45ccfc9a1988f913482289992257dae95c1b0d3f
    Author: MDwornikowski <marcin.m.dwornikowski@gmail.com>
    Date:   Fri Sep 1 22:05:44 2023 +0200
    
        Table of contents - changed the bottom lines color, added a hover handled by Js for each button in the Table of contents (to fix a hover bug on an active dropdown)
    
    commit ebef835920e34ca131c670455651e9c032580c56
    Author: MDwornikowski <marcin.m.dwornikowski@gmail.com>
    Date:   Thu Aug 31 00:35:02 2023 +0200
    
        Edited background color of .dropdown-button.active
    
    commit b859ca340d9c0cd561e1bc2d5c373e634a6eda99
    Author: MDwornikowski <marcin.m.dwornikowski@gmail.com>
    Date:   Thu Aug 31 00:29:27 2023 +0200
    
        Increased the indent paddings
    
    commit 64024ce7ba897f1c510f4e1e9e2f97c81d4538b4
    Author: MDwornikowski <marcin.m.dwornikowski@gmail.com>
    Date:   Thu Aug 31 00:27:13 2023 +0200
    
        Added Assets/Icons. Added styling and icons to the table of contents.
    
    commit 73212b476bc971e1c66bfd7dcbf205c2c45c8a9b
    Author: MDwornikowski <marcin.m.dwornikowski@gmail.com>
    Date:   Thu Aug 17 21:19:20 2023 +0200
    
        Cleaned up the code
    
    commit ab489e3a24bd1af3830b517c8a32f1b646736323
    Author: MDwornikowski <marcin.m.dwornikowski@gmail.com>
    Date:   Thu Aug 17 21:11:36 2023 +0200
    
        A simpler approach. Disabled 'position: absolute' on '.table-of-contents .nested-dropdown'.  JS: not calling 'calculateDropdownHeights();'.
    
    commit 3879233367ee97510c7113081688b4ad2174656d
    Author: MDwornikowski <marcin.m.dwornikowski@gmail.com>
    Date:   Thu Aug 17 21:08:07 2023 +0200
    
        Table of contents shenaningans
    
        Please don't mind the code
    
    commit 27aa9a080bbdaa72a01f74dfcb7d81c02809a479
    Author: MDwornikowski <marcin.m.dwornikowski@gmail.com>
    Date:   Tue Aug 15 11:47:49 2023 +0200
    
        Testing different approaches to applying proper margins in the table of contents nesting tree

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Toggle the burger menu
+    // Burger menu toggle
     const burgerMenuIcon = document.getElementById("burgerMenuIcon");
     const sideNavbar = document.getElementById("sideNavbar");
     const contentContainer = document.getElementById("contentContainer");
@@ -9,127 +9,50 @@ document.addEventListener("DOMContentLoaded", function () {
         contentContainer.classList.toggle("side-navbar-active");
     });
 
-    // Table of contents - Dropdown list
-    const items = document.getElementById("tableOfContents").querySelectorAll(".item");
-    const itemsWithDropdown = document.getElementById("tableOfContents").querySelectorAll('.has-dropdown');
-    var itemsWithDropdownDropdownHeights = [];
+    // Table of contents dropdowns
+    const tableOfContents = document.getElementById("tableOfContents");
+    const dropdowns = tableOfContents.querySelectorAll('.dropdown');
 
-    items.forEach(item => {
-        item.addEventListener("click", (event) => {
-            event.stopPropagation(); // Stop event propagation (childs calling the event on parents)
-        });
-    });
+    for (let i = 0; i < dropdowns.length; i++) {
+        const dropdown = dropdowns[i];
+        const nestedDropdown = dropdown.querySelector('.nested-dropdown');
+        const dropdownButton = dropdown.querySelector(".dropdown-button");
 
-    // itemsWithDropdown.forEach(item => {
-    //     const nestedDropdown = item.querySelector('.nested-dropdown');
+        const icon = dropdownButton.querySelector(".icon-arrow");
+        const iconInactivePath = "Assets/Icons/arrow-thin-right.png";
+        const iconActivePath = "Assets/Icons/arrow-thin-up.png";
 
-    //     var nextSibling = item.nextElementSibling;
-    //     if (nextSibling == null) {
-    //         nextSibling = getParentsNextSiblingRecursively(item);
-    //     }
-
-    //     item.querySelector(".dropdown-button").addEventListener('click', () => {
-    //         nestedDropdown.classList.toggle('active');
-
-    //         if (nestedDropdown.classList.contains('active')) {
-    //             nextSibling.style.marginTop = `${nestedDropdown.clientHeight}px`;
-    //         } else {
-    //             nextSibling.style.marginTop = '0';
-    //         }
-    //     });
-    // });
-
-    for (let i = 0; i < itemsWithDropdown.length; i++) {
-        const item = itemsWithDropdown[i];
-        const nestedDropdown = item.querySelector('.nested-dropdown');
-
-        // Add dropdown heights to their array:
-        const dropdownHeightObj = {
-            Item: item,
-            Height: `${nestedDropdown.clientHeight}px`
-        };
-        itemsWithDropdownDropdownHeights.push(dropdownHeightObj);
-
-        // Add 'click' event to itemsWithDropdown:
-        let nextSibling = item.nextElementSibling;
-        if (nextSibling == null) {
-            nextSibling = getParentsNextSiblingRecursively(item);
-        }
-
-        item.querySelector(".dropdown-button").addEventListener('click', () => {
+        dropdownButton.addEventListener('click', () => {
             nestedDropdown.classList.toggle('active');
 
             if (nestedDropdown.classList.contains('active')) {
-                // nextSibling.style.marginTop = `${nestedDropdown.clientHeight}px`;
-                console.log(nestedDropdown);
-                nextSibling.style.marginTop = getDropdownTotalHeightRecursively(nestedDropdown);
-            } else {
-                nextSibling.style.marginTop = '0';
+                icon.src = iconActivePath;
+                icon.alt = "Arrow up icon";
+
+                dropdownButton.classList.add('active');
+            }
+            else {
+                icon.src = iconInactivePath;
+                icon.alt = "Arrow down icon";
+
+                dropdownButton.classList.remove('active');
             }
         });
-    }
+    };
 
-    function getParentsNextSiblingRecursively(element) {
-        var parent = element.parentElement;
-        var nextSibling = parent.nextElementSibling;
-        if (nextSibling == null)
-            return getParentsNextSiblingRecursively(parent);
-        else
-            return nextSibling;
-    }
+    // Custom hover for buttons in the table of contents:
+    const tableOfContentsButtons = tableOfContents.querySelectorAll("button");
 
-    function getDropdownTotalHeightRecursively(dropdown) {
-        // var parent = item.parentElement;
-        //var totalHeight = `${item.querySelector('.nested-dropdown').clientHeight}px`;
-        // const parentNestedDropdown = parent.querySelector('.nested-dropdown')
-        // if (parentNestedDropdown != null) {
-        //     totalHeight += parentNestedDropdown;
-        //     totalHeight += getParentsDropdownHeightsRecursively(parent);
-        // }
-        // else {
-        //     return totalHeight;
-        // }
+    tableOfContentsButtons.forEach(function (button) {
+        let previousBackgroundColor;
 
-        var totalHeight = 0;
+        button.addEventListener("mouseover", function () {
+            previousBackgroundColor = button.style.backgroundColor;
+            button.style.backgroundColor = "rgba(0, 0, 124, 0.1)";
+        });
 
-        function traverseAndCalculateHeight(dropdown) {
-            totalHeight += dropdown.clientHeight;
-            var parent = dropdown.closest('.has-dropdown');
-            console.log(parent);
-            if (parent) {
-                var parentActiveDropdowns = [];
-                for (let i = 0; i < parent.children.length; i++) {
-                    var child = parent.children[i];
-                    if (child != dropdown) {
-                        if (child.classList.contains('nested-dropdown') && child.classList.contains('active')) {
-                            console.log(child);
-                            parentActiveDropdowns.push(child);
-                        }
-                    }
-                    else {
-                        break;
-                    }
-                }
-                console.log(parentActiveDropdowns.length);
-
-                if (parentActiveDropdowns) {
-                    for (let i = 0; i < parentActiveDropdowns.length; i++) {
-                        const parentActiveDropdown = parentActiveDropdowns[i];
-                        totalHeight += parentActiveDropdown.clientHeight;
-                    }
-                }
-
-                const parentNestedDropdown = parent.closest('.nested-dropdown');
-                if (parentNestedDropdown) {
-                    console.log(parentNestedDropdown);
-                    traverseAndCalculateHeight(parentNestedDropdown);
-                }
-
-            }
-        }
-
-        traverseAndCalculateHeight(dropdown);
-
-        return `${totalHeight}px`;
-    }
+        button.addEventListener("mouseout", function () {
+            button.style.backgroundColor = previousBackgroundColor;
+        });
+    })
 });
